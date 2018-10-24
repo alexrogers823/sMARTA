17f6587955caded9cac6e09426432f776b7a5b56
Sam Erickson <samerickson95@gmail.com>
2018-10-24 10:30:33 -0400

    moved example MARTA object into our api_examples folder

---
+++ api_examples/example_marta_object.js
@@ -0,0 +1,11 @@
+const exampleMARTA = {
+    "DESTINATION": "Hamilton E Holmes",
+    "DIRECTION": "W",
+    "EVENT_TIME": "10/23/2018 11:40:59 AM",
+    "LINE": "BLUE",
+    "NEXT_ARR": "11:41:09 AM",
+    "STATION": "ASHBY STATION",
+    "TRAIN_ID": "107206",
+    "WAITING_SECONDS": "-39",
+    "WAITING_TIME": "Boarding"
+},
