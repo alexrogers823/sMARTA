0a5c8c8f8e27c3fe64c999cbe3ada172a171ec62
Alex Rogers <alex.rogers823@gmail.com>
2018-10-26 10:49:43 -0400

    modified with new config file

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,16 @@
 // - Will's PapaParse.js
 // - CrimeData.js
 // - map_crime_data.js
+// - addCrimeToStation.js
 // - Sam's sam.js
 // - Sam's MARTA_times.js
 // - checkNeighborhood.js
 // - time_constaint.js
+
+const API = []
+
+
+Promise.all()
+.then('')
+.then('')
+.then('')
